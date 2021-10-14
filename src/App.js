c346e9194984a15b9d99f5c432dd8647ca5be4c0
Aleksei Koziatinskii <alekseik@netflix.com>
2021-10-13 19:03:26 -0700

    exercies 4: perspective decoration

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import FancyBackground from './components/FancyBackground';
 
 function App() {
   return (
     <>
+      <FancyBackground />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/FancyBackground/FancyBackground.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './FancyBackground.module.css';
+
+const FancyBackground = () => {
+  return (
+    <div className={styles.fancyBackground}>
+    </div>
+  );
+};
+
+export default FancyBackground;
